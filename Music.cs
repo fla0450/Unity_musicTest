50a9ca9f783737ecacc4ef898be29457de626d3e
fla0450 <94092857+fla0450@users.noreply.github.com>
2022-10-11 12:02:59 +0900

    musicplayer

---
+++ Music.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    AudioSource ad;
+    public AudioClip clip1;
+    public AudioClip clip2;
+    public AudioClip clip3;
+    public AudioClip clip4;
+    public AudioClip clip5;
+    public AudioClip clip6;
+    public AudioClip clip7;
+    public AudioClip clip8;
+    public AudioClip clip9;
+    public AudioClip clip10;
+    public AudioClip clip11;
+    public AudioClip clip12;
+    public AudioClip clip13;
+    public AudioClip clip14;
+    public AudioClip clip15;
+    public AudioClip clip16;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
